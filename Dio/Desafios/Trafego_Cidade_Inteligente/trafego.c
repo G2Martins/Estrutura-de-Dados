4848a41bd03e9bc82a3b482bf5ea2f049ec077d2
G2Martins <gustavogripaldi@outlook.com>
2024-04-03 15:25:24 -0300

    update cidade_trafego Files

--- Dio/Desafios/Trafego_Cidade_Inteligente/trafego.c
+++ Dio/Desafios/Trafego_Cidade_Inteligente/trafego.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-int mediaVeiculosHora(int tamanho, int veiculos[]) {
-  int i, soma = 0;
-  float media;
-  for (i = 0; i < tamanho; i++) {
-    soma += veiculos[i];
-  }
-  media = soma / tamanho;
-  media = round(media);
-  //TODO: Retornar a média arredondando o valor por meio da função "round".
-  return media;
+float mediaVeiculosHora(int tamanho, int veiculos[]) {
+    int i, soma = 0;
+    float media;
+    for (i = 0; i < tamanho; i++) {
+      soma += veiculos[i];
+    }
+    
+    media = soma / (float)tamanho; // Convertendo tamanho para float para garantir divisão correta
+    media = round(media);
+    
+    //TODO: Retornar a média arredondando o valor por meio da função "round".
+    return media;
 }
 
 int main() {
@@ -27,6 +29,6 @@ int main() {
     //TODO: Chamar a função "mediaVeiculosHora" e imprimir a saída de acordo com o enunciado.
     media = mediaVeiculosHora(tamanho, veiculos);
     
-    printf("Média de veículos por hora: %.f", media);
+    printf("Média de veículos por hora: %.f\n", media); // Correção na impressão do valor médio
     return 0;
 }
