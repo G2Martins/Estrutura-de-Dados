abc681a57c7ead10218591f718f3e26f0f9806b1
G2Martins <gustavogripaldi@outlook.com>
2024-05-12 21:03:22 -0300

    desafios files

--- Pilha/Desafios/provaP1/output/questao9.c
+++ Pilha/Desafios/provaP1/output/questao9.c
@@ -1,28 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define TAM 10
+#define TAM 1000
 
 char pilha[TAM];
-
-int topo=0;
+int topo = 0;
 
 char pop();
 int push(char x);
 
-int main(){
-    int qtd_teste;
+int main() {
+    int qtd_testes;
+    char *texto;
+    
+    scanf("%d", &qtd_testes);
 
-    scanf("%d", &qtd_teste);
+    while (qtd_testes--) {
+        texto = (char*)calloc(TAM, sizeof(char));
+        fgets(texto, TAM, stdin);
+        topo = 0;
 
+        int erro = 0;
+        for (size_t i = 0; i < strlen(texto) && !erro; i++) {
+            if (texto[i] == '(') {
+                push(texto[i]);
+            } else if (texto[i] == ')') {
+                if (pop() != '(') {
+                    erro = 1;
+                }
+            }
+        }
+
+        if (topo != 0)
+            erro = 1;
+
+        if (erro)
+            printf("NAO\n");
+        else
+            printf("SIM\n");
+
+        free(texto);
+    }
 
     return 0;
 }
 
-int push(char x)
-{
-    if (topo==TAM)
-    {
+int push(char x) {
+    if (topo == TAM) {
         return 0;
     }
 
@@ -31,14 +55,10 @@ int push(char x)
     return 1;
 }
 
-char pop()
-{
-    if (topo==0)
-    {
+char pop() {
+    if (topo == 0) {
         return ' ';
-    }
-    else
-    {
+    } else {
         topo--;
         return pilha[topo];
     }
